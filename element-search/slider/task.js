9ec2cca5633a60e3344ebe44d78d4c0cafa26ae5
manager7 <manager7@manager7.sandsoft.ru>
2019-12-26 16:46:46 +0300

    task.js added

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,30 +6,40 @@ let allPictures = document.querySelectorAll('.slider__item')
 
 let dots = document.querySelectorAll('.slider__dot')
 
-console.log(dots.length)
-
 function abc() {
 
-    let doc = document.querySelector('.slider__item_active')
+    let active = document.querySelector('.slider__item_active')
 
-    if (!doc.nextElementSibling) {
+    if (!active.nextElementSibling) {
 
-        doc.classList.remove('slider__item_active')
+        active.classList.remove('slider__item_active')
         allPictures[0].classList.add('slider__item_active')
 
     } else {
 
-        doc.nextElementSibling.classList.add('slider__item_active')
+        active.nextElementSibling.classList.add('slider__item_active')
 
-        doc.classList.remove('slider__item_active')
+        active.classList.remove('slider__item_active')
     }
 }
 
 nextClick.addEventListener('click', abc)
 previousClick.addEventListener('click', abc)
 
-for (let i of dots) {
-    i.addEventListener('click', function bce() {
 
-    })
+
+for (let i = 0; i < dots.length; i++) {
+
+    dots[i].addEventListener("click", function() {
+
+        let active = document.querySelector('.slider__item_active')
+
+        if(active){
+            active.classList.remove('slider__item_active')
+        }
+
+        allPictures[i].classList.add('slider__item_active')
+
+    });
 }
+
