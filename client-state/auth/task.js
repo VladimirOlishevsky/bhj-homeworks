8350ca7801521e21740aa0eeb987117ed5f9f549
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2020-01-17 21:14:20 +0300

    add homework

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,50 +2,39 @@ let signin = document.getElementById('signin');
 let form = document.getElementById('signin__form');
 let button = document.getElementById('signin__btn');
 let welcome = document.getElementById('welcome');
-let userId = document.getElementById('user_id');
+let inputFields = document.querySelectorAll('.control');
+
 
 signin.classList.add('signin_active');
 
 form.addEventListener('submit', function answer(e) {
- 
-    
 
     let formData = new FormData(form);
     let xhr = new XMLHttpRequest();
 
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-
-    //xhr.withCredentials = true;
-
     xhr.addEventListener('readystatechange', function response(e) {
 
-        let data = JSON.parse(xhr.responseText)
-        
-        if(xhr.readyState === xhr.DONE && data.success === true) {
-            localStorage.id = data.user_id;
+        let data = JSON.parse(xhr.responseText);
+        let userId = document.getElementById('user_id');
 
-            userId.value = data.user_id;
+        if (data.success === true) {
+
+            localStorage.id = data.user_id;
+            userId.textContent = localStorage.id;
+
             signin.classList.remove('signin_active');
             welcome.classList.add('welcome_active');
-            
-
-            console.log(userId.value)
-
-        } 
-        if(data.success === false) {
+        }
+        if (data.success === false) {
             signin.classList.remove('signin_active');
             alert('Неверный логин/пароль');
+            signin.classList.add('signin_active');
+            for (let i = 0; i < inputFields.length; i++) {
+                inputFields[i].value = '';
+            }
         }
-        // if (xhr.readyState == xhr.DONE && xhr.status == 200) {
-        //     var data = JSON.parse(xhr.responseText);
-        //     console.log(data);
-        // } else {
-            
-        //     console.log('errr')
-        // }
-
     })
     xhr.send(formData);
     e.preventDefault();
-    
 })
