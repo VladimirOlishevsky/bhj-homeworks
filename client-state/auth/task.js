23a70cb0f9f2f9cd108d78447a75d6db0ff839d5
VladimirOlishevsky <vova.olishevskij@inbox.ru>
2020-01-18 19:15:32 +0300

    add task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -13,29 +13,26 @@ form.addEventListener('submit', function answer(e) {
     let xhr = new XMLHttpRequest();
 
     xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    xhr.addEventListener('readystatechange', function response(e) {
+    xhr.addEventListener('load', function response(e) {
 
-        if ((xhr.readyState === 4 && xhr.status === 200)) {
+        let data = JSON.parse(xhr.responseText);
+        let userId = document.getElementById('user_id');
 
-            let data = JSON.parse(xhr.responseText);
-            let userId = document.getElementById('user_id');
+        if (data.success === true) {
 
-            if (data.success === true) {
+            localStorage.id = data.user_id;
+            userId.textContent = localStorage.id;
 
-                localStorage.id = data.user_id;
-                userId.textContent = localStorage.id;
+            signin.classList.remove('signin_active');
+            welcome.classList.add('welcome_active');
+        }
+        if (data.success === false) {
+            signin.classList.remove('signin_active');
+            alert('Неверный логин/пароль');
 
-                signin.classList.remove('signin_active');
-                welcome.classList.add('welcome_active');
-            }
-            if (data.success === false) {
-                signin.classList.remove('signin_active');
-                alert('Неверный логин/пароль');
-
-                signin.classList.add('signin_active');
-                for (let i = 0; i < inputFields.length; i++) {
-                    inputFields[i].value = '';
-                }
+            signin.classList.add('signin_active');
+            for (let i = 0; i < inputFields.length; i++) {
+                inputFields[i].value = '';
             }
         }
     })
